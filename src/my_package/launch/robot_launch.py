50605f84446bb7fd2dce61291aafd3792b92e949
Adrian Riadi <34883039+corvus-rex@users.noreply.github.com>
2025-05-17 23:37:42 +0700

    Added obstacle avoider

--- src/my_package/launch/robot_launch.py
+++ src/my_package/launch/robot_launch.py
@@ -1,5 +1,6 @@
 import os
 import launch
+from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
@@ -9,7 +10,6 @@ from webots_ros2_driver.webots_controller import WebotsController
 def generate_launch_description():
     package_dir = get_package_share_directory('my_package')
     robot_description_path = os.path.join(package_dir, 'resource', 'my_robot.urdf')
-    print(package_dir)
 
     webots = WebotsLauncher(
         world=os.path.join(package_dir, 'worlds', 'my_world.wbt')
@@ -22,9 +22,15 @@ def generate_launch_description():
         ]
     )
 
+    obstacle_avoider = Node(
+        package='my_package',
+        executable='obstacle_avoider',
+    )
+
     return LaunchDescription([
         webots,
         my_robot_driver,
+        obstacle_avoider,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
